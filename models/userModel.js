e32c882efe9e2eb7931760e3ad8a4b5d9eadb5e6
AyushAwasthi52 <ayushawasthi2205@gmail.com>
2025-12-14 11:27:38 +0530

    St

---
+++ models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const validator = require('validator')
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A user must have a name"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "A user must have an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email address']
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Passwords should match"],
+  },
+});
+
+const userModel = new mongoose.Model("User", userSchema);
+
+module.exports = userModel;
