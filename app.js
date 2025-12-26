44a60821c36e8da65b837ee9125b63d485b2380e
AyushAwasthi52 <ayushawasthi2205@gmail.com>
2025-12-26 11:11:30 +0530

    View

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -13,6 +14,11 @@ const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // 1) MIDDLEWARES
 app.use(helmet());
@@ -47,7 +53,6 @@ app.use(
 app.set("query parser", "extended");
 
 app.use(express.json({ limit: "10kb" }));
-app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -55,6 +60,10 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.use("/", (req, res) => {
+  res.status(200).render("base");
+});
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
